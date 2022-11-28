12b661c80754e0be1916defb0bab32323ef53987
Toni <antoniserrasastre@gmail.com>
2022-11-28 20:35:29 +0100

    ACTUALIZACION A 28/11

--- src/PaginaCategorias.py
+++ src/PaginaCategorias.py
@@ -4,7 +4,7 @@ import json
 
 #Guardamos la informacion de html.content en html_file y lo abrimos como index.html
 def Html_py(items):
-
+    #título
     html_content = f"""
      <!DOCTYPE html>
     <html>
@@ -39,7 +39,7 @@ def Html_py(items):
         <text text-anchor="middle" x="50" y="15" font-size="17" fill="url(#gradient)" fill-opacity="0.1">RENT BIKE</text>
         </svg>
     """
-
+    #recoremos los json 
     for item in items:
 
         html_content +="""
@@ -97,25 +97,65 @@ def CrearPaginasIndividuales(items):
         </svg>
 
         <div id="contenedor">
-            <div class="A">
-                <div class=""> 
-                    <img class="img" src="https://contents.mediadecathlon.com/p2091636/k$cc0790528e1a07724f38362c6dc52705/sq/bicicleta-de-montaa-29-aluminio-ntt-sport-60-rojo.jpg?format=auto&f=800x0">
-                    <p id="divText">{item.get('brand')}:{item.get('model')}</p>
+            <a id="link" href="../PaginasTablas/{item.get('serial')}.html">
+                <div class="A">
+                    <div class=""> 
+                        <img class="img" src="https://contents.mediadecathlon.com/p2091636/k$cc0790528e1a07724f38362c6dc52705/sq/bicicleta-de-montaa-29-aluminio-ntt-sport-60-rojo.jpg?format=auto&f=800x0">
+                        <p id="divText">{item.get('brand')}:{item.get('model')}</p>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
     </body>
     </html>"""
 
 
-        escribirHTML("PaginasIndividuales/"+item.get('serial'),html_content) 
-    
+    escribirHTML("PaginasIndividuales/" + item.get('serial'), html_content) 
+        
+def TablaHtml(element):
+    # element = items[0]
+    aux = """
+    <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta lang="en">
+                <title>BIKER'S CLUB</title>
+            </head>
+            <body>
+                <section id="bicicletas_carretera">
+                    <h1> Professional Road Bikes</h1>
+                    <p>
+                        <table class="default">
+                            <colgroup>
+                                <col span="1" style="background-color: #79b8db">
+                            </colgroup>
+    """
+
+    for k, v in element.items():
+        aux += """
+                        <tr>
+                            <td><b>%s</b></td>
+                            <td>%s</td>
+                        </tr>
+        """ % (k, v)
+
+    aux += """
+        </table>
+                    <img src="https://www.canyon.com/on/demandware.static/-/Sites-canyon-master/default/dwfdcff3fc/images/full/full_2022_/2022/full_2022_ultimate-cf-sl-7-disc_3072_gy-bk_P5.png" alt="foto Ultimate CF SL 7 Disc" height="100px" weight="px">
+                </p>
+            </section>
+        </body>
+    </html>
+    """
+    escribirHTML("PaginasTablas/"+ element.get('serial'),aux)
 
 def escribirHTML(nombre, contenido):
     
     with open("html/" + nombre + ".html", "w+") as html_file:
         html_file.write(contenido)
-        print ("HTML file created successfully")
+        print ("HTML file " + nombre + " created successfully")
 
 #Guardamos el diccionario como una variable
 #recorremos el diccionario para extraer el valor de una 'Key' y la guardamos en una variable.
@@ -127,11 +167,18 @@ def cargarDatos(ruta='DataBase/AllBikes.json'):
 
 
 if __name__== '__main__':
+    from ApiMongoDB import ApiMongoDB
+    ApiMongoDB()
+    
     items = cargarDatos()
     
     Html_py(items)
 
     CrearPaginasIndividuales(items)
+    
+    for item in items:
+        TablaHtml(item)
+    # TablaHtml(items)
 
     
 
